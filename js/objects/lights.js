e7778ba1c7eca67f7352f4f6dac6c617f2177418
kindOfCurly <marie.sautmann@gmx.de>
2018-09-13 22:14:49 +0200

    player start position

--- js/objects/lights.js
+++ js/objects/lights.js
@@ -2,19 +2,19 @@ function getDirectionalLights() {
   let directionals = new THREE.Group();
 
   let light1 = new THREE.DirectionalLight( 0xa0a0ff, 1.5 );
-  light1.position.set(25, 8, 0);
+  light1.position.set(25, 10, 0);
   light1.castShadow = true;
 
   let light2 = new THREE.DirectionalLight( 0xa0a0ff, 1.5 );
-  light2.position.set(0, 8, 25);
+  light2.position.set(0, 10, 25);
   light2.castShadow = true;
 
   let light3 = new THREE.DirectionalLight( 0xa0a0ff, 1.5 );
-  light3.position.set(-25, 8, 0);
+  light3.position.set(-25, 10, 0);
   light3.castShadow = true;
 
   let light4 = new THREE.DirectionalLight( 0xa0a0ff, 1.5 );
-  light4.position.set(0, 8, -25);
+  light4.position.set(0, 10, -25);
   light4.castShadow = true;
 
   directionals.add(light1);
