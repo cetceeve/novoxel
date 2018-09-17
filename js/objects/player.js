e61bb592fb1e61503fcd953b638a06a78b04d9a3
Fabian Zeiher <fzeiher@gmail.com>
2018-09-17 18:15:55 +0200

    REFACTOR: optimize player class

--- js/objects/player.js
+++ js/objects/player.js
@@ -24,47 +24,39 @@ class Player {
   }
 
   getRepresentation() {
-    console.log(this.dimension);
-    let bottomGeometry = new THREE.SphereGeometry(this.dimension, 16, 16);
-    let bottomMaterial = new THREE.MeshLambertMaterial({
-      color: 0x2d3e50
-    });
+    let bottom, cyl, ball, playerRep;
 
-    let bottom = new THREE.Mesh(bottomGeometry, bottomMaterial);
-    bottom.receiveShadow = true;
-    bottom.castShadow = true;
-    bottom.position.y = 0.7;
+    bottom = new THREE.Mesh(
+      new THREE.SphereGeometry(this.dimension, 16, 16),
+      new THREE.MeshLambertMaterial({ color: 0x2d3e50 })
+    );
 
-    let cylGeometry = new THREE.CylinderGeometry(0.2, 0.5, 1, 32, 1, false);
-    let cylMaterial = new THREE.MeshLambertMaterial({
-      color: 0x2d3e50
-    });
+    cyl = new THREE.Mesh(
+      new THREE.CylinderGeometry(0.2, 0.5, 1, 32, 1, false),
+      new THREE.MeshLambertMaterial({ color: 0x2d3e50 })
+    );
+    cyl.position.y = 0.5;
 
-    let cyl = new THREE.Mesh(cylGeometry, cylMaterial);
-    cyl.receiveShadow = true;
-    cyl.castShadow = true;
-    cyl.position.y = 1.2;
+    ball = new THREE.Mesh(
+      new THREE.SphereGeometry(0.4, 16, 16),
+      new THREE.MeshLambertMaterial({ color: 0x2d3e50 })
+    );
+    ball.position.y = 1.5;
 
-    let ballGeometry = new THREE.SphereGeometry(0.4, 16, 16);
-    let ballMaterial = new THREE.MeshLambertMaterial({
-      color: 0x2d3e50
-    });
+    playerRep = new THREE.Group();
+    playerRep.add(cyl);
+    playerRep.add(ball);
+    playerRep.add(bottom);
 
-    let ball = new THREE.Mesh(ballGeometry, ballMaterial);
-    ball.receiveShadow = true;
-    ball.castShadow = true;
-    ball.position.y = 2.2;
-
-    let player = new THREE.Group();
-    player.add(cyl);
-    player.add(ball);
-    player.add(bottom);
-
-    return player;
+    for (let i = 0; i < playerRep.children.length; i++) {
+      playerRep.children[i].castShadow = true;
+      playerRep.children[i].receiveShadow = true;
+    }
+    return playerRep;
   }
 
   getYHitDetectionDistance() {
-    return this.dimension * 0.4;
+    return this.dimension * 1.2;
   }
 
   updatePosition(gravityDistance, moveDistance) {
